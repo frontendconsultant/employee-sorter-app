ab14191134e47277ca80d2367f2b0a8e06fa37cc
vijay-kumar-singh <vijaysingh.nl@gmail.com>
2020-02-27 00:54:37 +0100

    adding  unit test

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -11,19 +11,22 @@ class AppContainer extends Component {
       this.state = {
         error: null,
         isLoaded: false,
-        items: []
+        items: [],
+        changed:false
       };
+      this.onClick = this.onClick.bind(this);
+      this.myRef = React.createRef();
     }
   
     componentDidMount() {
-      const url = 'https://jsonplaceholder.typicode.com/users' || config.userUrl + '?results=' + config.numberCards + '&nat=US';
+      const url = config.userUrl + '?results=' + config.numberCards + '&nat=US';
       fetch(url)
         .then(res => res.json())
         .then(
           (result) => {
             this.setState({
               isLoaded: true,
-              items: this.buildObject(result.results||result)
+              items: this.buildObject(result.results)
             });
           },
           // Note: it's important to handle errors here
@@ -39,7 +42,7 @@ class AppContainer extends Component {
     }
 
     buildObject(result) {
-      const sortedItems = result.sort((a, b) => (a.name || a.name.first > b.name ||  b.name.first ? 1 : -1));
+      const sortedItems = result.sort((a, b) => (a.name.first > b.name.first ? 1 : -1));
       const filteredMappedItems = config.tabs.map(item => {
         item = this.filterItems(item, sortedItems);
         return item;
@@ -48,18 +51,54 @@ class AppContainer extends Component {
       return filteredMappedItems;
     }
 
+    addProperty(item) {  
+      const addedPropItem = item.map(val => {
+         return {...val, isOpen:false};
+      });
+      return addedPropItem
+    }
+
     filterItems(alphabet, sortedObject) {
       let alphabetObject = {};
-      let filterdObj = sortedObject.filter(item =>  (item.name || item.name.first).charAt(0).toLowerCase() === alphabet);
+      let filterdObj = sortedObject.filter(item =>  (item.name.first).charAt(0).toLowerCase() === alphabet);
       alphabetObject.alphabet = alphabet;
-      alphabetObject.itemCollection = filterdObj;
+      alphabetObject.itemCollection = this.addProperty(filterdObj);
       return alphabetObject;
 
+    }
+    onClick(selectedname) {
+      const {items} = this.state;
+      const nameKey = selectedname.split(',')[1].charAt(1).toLowerCase();
+
+      const selectedItem =  items.filter(item => (item.alphabet === nameKey));
+      const updatedItem = selectedItem[0].itemCollection.map(value => {
+        if(value.name.last.toLowerCase() === this.trimString(selectedname.split(',')[0].toLowerCase()) && 
+        value.name.first.toLowerCase() === this.trimString(selectedname.split(',')[1].toLowerCase()) ) {
+          return {...value, isOpen:true}
+        }
+        return {...value, isOpen:false}
+        } );
+ 
+        const finalObj = this.state.items.map(val => {
+          if(val.alphabet === nameKey) {
+            return {...val, itemCollection:updatedItem}
+          }
+          return val;
+        });
+          this.setState({isLoaded: true,
+            items: finalObj,
+           changed:true});       
+    }
+
+
+    trimString(str) {
+      const strValue = str.split(' ').join('');
+      return strValue;
     }
   
     render() {
       const { error, isLoaded, items } = this.state;
-    
+      
       if (error) {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
@@ -70,15 +109,16 @@ class AppContainer extends Component {
         return (
           <Tabs>
             {items.map(item => (
-              <div label={item.alphabet} count={item.itemCollection.length}>
+              <div label={item.alphabet} count={item.itemCollection.length} ref={this.myRef}>
                  {
                    item.itemCollection.map( (coll, index) => (
-                    <ExpandableList title={`${coll.name || coll.name.last}, ${coll.name || coll.name.first}`}>
+          
+                    <ExpandableList title={`${coll.name.last}, ${coll.name.first}`} isOpen={coll.isOpen} onClick={this.onClick}>
                     <ContactItem
                       key={index}
-                      name={`${coll.name || coll.name.last}, ${(coll.name || coll.name.first).toUpperCase()}`}
+                      name={`${coll.name.last}, ${(coll.name.first).toUpperCase()}`}
                       email={coll.email}
-                      //*pic={coll.picture.thumbnail || ''}
+                      pic={coll.picture.thumbnail}
                     />
                   </ExpandableList>
                    ))
--- src/components/ExpandableList.js
+++ src/components/ExpandableList.js
@@ -3,23 +3,27 @@ import Chevron from "./Chevron";
 
 import "./ExpandableList.css";
 
-const ExpandableList = ({ title, children }) => {
+const ExpandableList = ({ title, children, isOpen ,onClick }) => {
   const [setActive, setActiveState] = useState("");
   const [setHeight, setHeightState] = useState("0px");
   const [setRotate, setRotateState] = useState("expandableList__icon");
-
+  
   const content = useRef(null);
 
+ 
   const toggleExpandableList = () => {
+    onClick(title);
     setActiveState(setActive === "" ? "active" : "");
     setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`);
+    //setHeightState(!isOpen ? "0px" : `${content.current.scrollHeight}px`)
     setRotateState(
       setActive === "active"
         ? "expandableList__icon"
         : "expandableList__icon rotate"
     );
+    
   }
-
+  console.log(isOpen)
   return (
     <div className="expandableList__section">
       <button
